cb9f6cf1bb5426a038f1d1aa4b6e9eb3f89be945
Crizan Cesar <crizan@icloud.com>
2025-11-12 15:47:44 -0300

    alt geral

--- assets/js/comissoes.js
+++ assets/js/comissoes.js
@@ -3,57 +3,45 @@ const arrComissions = [
         title: 'COMISSÃO DE PRERROGATIVAS',
         presidente: 'Dra. Tatiane Cristina Camargo Ferreira',
         emailPresidente: 'tccamargof@gmail.com',
-        vicePresidente: 'Dra. Fabiana Morais Alves',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dr. Giulio Henrique Garcia Fontes Gatti',
+            'Dra. Sarah Cristina Duarte Fortis ',
             'Dr. Ricardo Benedicto Martins',
-            'Dra. Nathalia Loures Capelo'
         ],
     },
     {
         title: 'COMISSÃO PERMANENTE DAS MULHERES ADVOGADAS',
-        presidente: 'Dra. Claudinéia de Fátima da Silva',
+        presidente: 'Dra. Beatriz De Almeida Neves ',
         emailPresidente: 'claudineia.adv@hotmail.com',
-        vicePresidente: 'Dra. Lurdete Vendrame',
+        vicePresidente: null,
         instagramLink: 'https://www.instagram.com/comissaomulheradvogadajandira/',
         membrosEfetivos: [
-            'Dra. Adreiza Farias de Oliveira',
-            'Dra. Adriana Barreira de Araújo',
-            'Dra. Carolina Gonçalves',
-            'Dra. Caroline Ingrid de Lima Gregório',
-            'Dra. Cristiane Santos de Jesus',
-            'Dra. Fernanda Scopim',
-            'Dra. Flávia Lima da Silva Laurenti',
-            'Dra. Heloiza Galvão',
-            'Dra. Joingle Oliveira Lima',
-            'Dra. Juliana dos Santos Menezes',
-            'Dra. Luísa Fernanda de Lima',
-            'Dra. Marcela Paula Minjoni',
-            'Dra. Michelly Alves Bezerra de Oliveira',
-            'Dra. Sarah Cristina Duarte Fortis',
-            'Dra. Stefany Priscila de Araújo',
-            'Dra. Tatiane Cristina Camargo Ferreira',
-            'Dra. Verônica de Souza Domingos Verga'
+            'Dra. Barbara Jaqueline da Fonseca Valério ',
+            'Dra. Isabela  de Almeida Dias ',
+            'Dra. Marcela Paula Minjoni ',
+            'Dra. Sarah Sanches Goudinho '
         ],
     },
     {
         title: 'COMISSÃO DE DIREITO EMPRESARIAL',
         presidente: 'Dr. Heraldo da Silva Azevedo Junior',
         emailPresidente: 'heraldodsaj@gmail.com',
-        vicePresidente: 'Dr. Marcos Antonio Picoli',
+        vicePresidente: null,
         instagramLink: 'https://www.instagram.com/direitoempresarial_oab.jandira/',
         membrosEfetivos: [
-            'Dr. Giovani Roberto Zitelli',
-            'Dr. Alexandre Alves Neto',
+            'Dr. Marco Antonio Moreira ',
             'Dr. Jhonatan Rodrigues do Carmo'
         ],
     },
     {
         title: 'COMISSÃO DA ASSISTÊNCIA JUDICIÁRIA',
-        presidente: 'Dr. Renato Pereira Gomes',
+        presidente: 'Dr. Person De Sousa Lima  ',
         emailPresidente: 'renato.gomes@adv.oabsp.org.br',
-        vicePresidente: 'Dr. Adriano Borges Nogueira',
-        membrosEfetivos: [],
+        vicePresidente: 'Dra. Tatiane Cristina Camargo Ferreira',
+        membrosEfetivos: [
+            'Dr. Marcos Aurélio Santos ',
+            'Dr. Valcir Moura Leal '
+        ],
     },
     {
         title: 'COMISSÃO DE ESPORTE SAÚDE E LAZER',
@@ -61,21 +49,17 @@ const arrComissions = [
         emailPresidente: 'cris_juris@hotmail.com',
         instagramLink: 'https://www.instagram.com/comissao_de_esportes_oabjandir/',
         membrosEfetivos: [
-            'Dr. Anderson Oliveira da Silva',
-            'Dra. Jaqueline de Souza Silva',
-            'Dr. Samuel Lima da Silva',
-            'Dr. William dos Santos Ribeiro',
-            'Dra. Jessica de Lima Santos'
+            'Dr. William dos Santos Ribeiro'
         ],
     },
     {
         title: 'COMISSÃO DE REGULARIZAÇÃO FUNDIÁRIA',
-        presidente: 'Dra. Heloiza Galvão',
+        presidente: 'Dr. William dos Santos  Ribeiro ',
         emailPresidente: 'heloizagalvao0820@gmail.com',
-        vicePresidente: 'Dra. Luciana de Araújo Blum',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Priscila Aparecida Foger',
-            'Dr. Bruno Aparecido da Silva'
+            'Dra. Valeria Maia Meduneckas Tourinho ',
+            'Dra. Vitória Beatriz Estrela Alves de Lima'
         ],
     },
     {
@@ -83,73 +67,58 @@ const arrComissions = [
         presidente: 'Dr. Alexandre Alves Neto',
         emailPresidente: 'alexandrealvesneto@gmail.com',
         instagramLink: 'https://www.instagram.com/oabcyber/',
-        vicePresidente: 'Dr. Heraldo da Silva Azevedo Junior',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Lilian Meire Cambuim Alves'
+            null
         ],
     },
     {
-        title: 'COMISSÃO DE ORATÓRIA E CERIMONIAL',
-        presidente: 'Dra. Marcela de Paula Minjoni',
-        emailPresidente: 'marcelapaula@adv.oabsp.org.br',
-        membrosEfetivos: [
-            'Dr. Giovani Roberto Zitelli'
-        ],
-    },
-    {
-        title: 'COMISSÃO PENAL E DIREITO PENAL',
+        title: 'COMISSÃO DE DIREITO PENAL E PROCESSO PENAL',
         presidente: 'Dr. Valcir Moura Leal',
         emailPresidente: 'valcirmoura@adv.oabsp.org.br',
-        vicePresidente: 'Dra. Michelly Alves Bezerra de Oliveira',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Caroline Ingrid de Lima Gregório',
-            'Dr. Anderson Oliveira da Silva',
-            'Dra. Sarah Cristina Duarte Fortis',
-            'Dra. Tatiane Cristina Camargo Ferreira',
-            'Dra. Priscila da Silva'
+            'Dr. Caio Julio Cesar Santos Simões de Almeida',
+            'Dr. Elias Tavares de Lima',
+            'Dr. Marcos Aurélio Santos'
         ],
     },
     {
         title: 'COMISSÃO DA JOVEM ADVOCACIA',
-        presidente: 'Dra. Fernanda Scopim',
+        presidente: 'Dra. Isabela  de Almeida Dias ',
         emailPresidente: 'fscopim@gmail.com',
-        vicePresidente: 'Dr. Anderson Oliveira da Silva',
+        vicePresidente: null,
         instagramLink: 'https://www.instagram.com/jovem.adv.jandira/',
         membrosEfetivos: [
-            'Dra. Caroline Ingrid de Lima Gregório',
-            'Dra. Daniele Aparecida dos Santos Silva',
-            'Dra. Denise Lopes dos Santos',
-            'Dr. Kauê Teixeira Ventura Monteiro',
-            'Dra. Sarah Cristina Duarte Fortis',
-            'Dra. Sara Raiane de Souza Alves Barbosa',
-            'Dra. Priscila da Silva',
+            'Dra. Gabriella Alves de Sant Ana  ',
+            'Dra. Sarah Evangelista Justiniano ',
+            'Dra. Sueila Silva Carvalho '
         ],
     },
     {
         title: 'COMISSÃO DE CULTURA E EVENTOS',
-        presidente: 'Dra. Carolina Gonçalves',
+        presidente: 'Dra. Cristiane Santos de Jesus ',
         emailPresidente: 'carol2609@gmail.com',
+        vicePresidente: 'Dr. William dos Santos  Ribeiro ',
         membrosEfetivos: [],
     },
     {
         title: 'COMISSÃO DE DIREITO DO TRABALHO',
-        presidente: 'Dr. Leandro Aparecido da Silva',
+        presidente: null,
         emailPresidente: 'leandro_haurios@hotmail.com',
-        vicePresidente: 'Dr. José Carlos Poletto Junior',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dr. Natanael Conceição dos Santos',
-            'Dra. Barbara Jaqueline da Fonseca Valério',
-            'Dra. Karoline de Lima Silva',
+            'Dra. Sarah Sanches Goudinho',
             'Dra. Lucineide de Araujo Castilho'
         ],
     },
     {
         title: 'COMISSÃO DE DIREITOS HUMANOS',
-        presidente: 'Dr. Anderson Oliveira da Silva',
+        presidente: 'Dra. Isabela  de Almeida Dias ',
         emailPresidente: 'anderson.oliveiraadv22@gmail.com',
         instagramLink: 'https://www.instagram.com/com.direitoshumanosdejandira/',
         membrosEfetivos: [
-            'Dra. Tatiane Cristina Camargo Ferreira'
+            'Dra. Sarah Sanches Goudinho'
         ],
     },
     {
@@ -157,9 +126,12 @@ const arrComissions = [
         presidente: 'Dra. Barbara Jaqueline da Fonseca Valério',
         emailPresidente: 'babijaqueline@hotmail.com',
         instagramLink: 'https://www.instagram.com/cirjandira/',
-        vicePresidente: 'Dra. Sarah Evangelista Justiniano',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Denise Lopes dos Santos'
+           'Dra. Denise Lopes dos Santos  ',
+           'Dra. Marcela Paula Minjoni ',
+           'Dr. Norberto Gil Ribeiro ',
+           'Dra. Sarah Evangelista Justiniano '
         ],
     },
     {
@@ -168,105 +140,99 @@ const arrComissions = [
         emailPresidente: 'dralucianescambuim@gmail.com',
         instagramLink: 'https://www.instagram.com/cdpi_oabjandira/',
         membrosEfetivos: [
-            'Dra. Lilian Meire Cambuim Alves'
+            'Dra. Lilian Meire Cambuim Alves',
+            'Dr. Elson Rochane Neves ',
+            'Dra. Nathalia Loures Capelo '
         ],
     },
     {
         title: 'COMISSÃO DE DEFESA DOS ANIMAIS',
-        presidente: 'Dra. Tatiane Cristina Camargo Ferreira',
+        presidente: 'Dra. Michelly Alves Bezerra  de Oliveira ',
         emailPresidente: 'tccamargof@gmail.com',
-        vicePresidente: 'Dra. Michelly Alves Bezerra de Oliveira',
+        vicePresidente: 'Dra. Sarah Evangelista Justiniano ',
         instagramLink: 'https://www.instagram.com/comissaodefesadosanimais/',
         membrosEfetivos: [
-            'Dr. Carlos Henrique Paulino',
-            'Dra. Sarah Evangelista Justiniano',
+            'Dr. Edson Morais da Cunha ',
             'Dra. Nathalia Loures Capelo'
         ],
     },
     {
         title: 'COMISSÃO DA OAB VAI À ESCOLA',
-        presidente: 'Dr. Anderson Ferreira',
+        presidente: 'Dr. Samuel de Paula Oliveira ',
         emailPresidente: 'wanderreira@gmail.com',
-        vicePresidente: 'Dra. Denise Lopes dos Santos',
+        vicePresidente: 'Dr. Adalberto Alexandre Santos ',
         membrosEfetivos: [
-            'Dr. Adalberto Alexandre Santos',
-            'Dr. Edemar dos Santos Silva Filho',
-            'Dra. Helena Cristina Caldeira Trindade Estevão'
+            'Dra. Ayeska Lorena Lopes Pereira ',
+            'Dra. Gabriella Alves de Sant Ana  ',
+            'Dra. Lilian Meire Cambuim Alves ',
+            'Dra. Luciane Sandra Cambuim ',
+            'Dra. Marcela Paula Minjoni '
         ],
     },
     {
         title: 'COMISSÃO DE ÉTICA E DISCIPLINA',
-        presidente: 'Dra. Carolina Gonçalves',
+        presidente: 'Dra. Leni Antonia da Silva Aguiar ',
         emailPresidente: 'carol2609@gmail.com',
-        vicePresidente: 'Dr. Ricardo Benedicto Martins',
-        membrosEfetivos: [],
+        vicePresidente: null,
+        membrosEfetivos: [
+            'Dr.Elson Rochane Neves ',
+            'Dra. Luciane Sandra Cambuim',
+            'Dr. Ricardo Benedicto Martins ',
+            'Dra. Tatiane Cristina Camargo Ferreira ',
+            'Dr. William dos Santos  Ribeiro '
+        ],
     },
     {
         title: 'COMISSÃO DE DIREITO PREVIDENCIÁRIO',
-        presidente: 'Dr. Samuel Lima da Silva',
+        presidente: 'Dra. Sueila Silva Carvalho ',
         emailPresidente: 'reisamuellima@gmail.com',
-        vicePresidente: 'Dra. Michelly Alves Bezerra de Oliveira',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Sarah Cristina Duarte Fortis',
-            'Dra. Magna de Lima Galvão',
+            'Dr. Elias Tavares de Lima ',
+            'Dra. Jaqueline Renata dos Santos de Oliveira ',
             'Dra. Iolanda Santana de França Lima'
         ],
     },
     {
         title: 'COMISSÃO DE DIREITO ELEITORAL',
-        presidente: 'Dr. Geizon Branquinho do Nascimento',
+        presidente: 'Dra. Claudinéia de Fátima da Silva ',
         emailPresidente: 'dr.branquinho@adv.oabsp.org.br',
-        vicePresidente: 'Dra. Claudinéia de Fátima da Silva',
+        vicePresidente: null,
         membrosEfetivos: [],
     },
     {
         title: 'COMISSÃO DE DIREITO ADMINISTRATIVO',
-        presidente: 'Dr. Natanael Conceição dos Santos',
+        presidente: 'Dr. Ricardo Benedicto Martins - ',
         emailPresidente: 'natalaelegos@gmail.com',
         membrosEfetivos: [
-            'Dr. Jhonatan Rodrigues do Carmo'
+            'Dr. Norberto Gil Ribeiro '
         ],
     },
     {
-        title: 'COMISSÃO DE DIREITO AMBIENTAL',
-        presidente: 'Dr. Luiz Carlos de Souza Filho',
-        emailPresidente: 'luizscopim@adv.oabsp.org.br',
-        vicePresidente: 'Dra. Lúcia Simões de Almeida Morais',
-        membrosEfetivos: [],
-    },
-    {
         title: 'COMISSÃO DE DIREITO TRIBUTÁRIO',
-        presidente: 'Dr. Samuel de Paula Oliveira',
+        presidente: 'Dra. Marluce Batista Matos ',
         emailPresidente: 'sp.oliveira@adv.oabsp.org.br',
-        vicePresidente: 'Dra. Adreiza Farias de Oliveira',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Marluce Batista Matos',
-            'Dr. Jhonatan Rodrigues do Carmo',
-            'Dra. Jherelle Aparecida Marques'
+            'Dra. Ayeska Lorena Lopes Pereira ',
+            'Dr. Elias Tavares de Lima '
         ],
     },
     {
         title: 'COMISSÃO DA AÇÃO SOCIAL INFÂNCIA E JUVENTUDE',
-        presidente: 'Dra. Adreiza Farias de Oliveira',
+        presidente: 'Dr. João Batista Alves Pereira ',
         emailPresidente: 'adreiza@adv.oabsp.org.br',
         membrosEfetivos: [],
     },
     {
         title: 'COMISSÃO DE DIREITO CIVIL',
-        presidente: 'Dr. Ricardo Benedicto Martins',
+        presidente: 'Dr. João Batista Alves Pereira ',
         emailPresidente: 'ricardobmartins@adv.oabsp.org.br',
         membrosEfetivos: [
-            'Dr. Giovani Roberto Zitelli',
-            'Dra. Jherelle Aparecida Marques',
+            'Dra. Vitória Beatriz Estrela Alves de Lima',
             'Dra. Lucineide de Araujo Castilho',
             'Dr. Jhonatan Rodrigues do Carmo'
         ],
-    },
-    {
-        title: 'COMISSÃO DE SEGURANÇA PÚBLICA',
-        presidente: 'Dr. Elias Tavares de Lima',
-        emailPresidente: 'elieli@outlook.com',
-        membrosEfetivos: [],
     },
     {
         title: 'COMISSÃO DE TECNOLOGIA',
@@ -274,33 +240,31 @@ const arrComissions = [
         membrosEfetivos: [],
     },
     {
-        title: 'COMISSÃO DE HELPDESK',
-        presidente: 'Dra. Elaine Carvalho de Aquino',
-        emailPresidente: 'draelaineaquino@gmail.com',
-        vicePresidente: 'Dr. Renato Pereira Gomes',
-        membrosEfetivos: [],
-    },
-    {
         title: 'COMISSÃO DE DIREITO DE FAMÍLIA',
-        presidente: 'Dra. Priscila Aparecida Foger',
+        presidente: 'Dra. Beatriz de Almeida Neves',
         emailPresidente: 'foger2409@gmail.com',
-        vicePresidente: 'Dra. Luciana de Araújo Blum',
+        vicePresidente: null,
         membrosEfetivos: [
-            'Dra. Jherelle Aparecida Marques'
+            'Dra. João Batista Alves Pereira',
+            'Dra. Lucineide de Araujo Castilho ',
+            'Dra. Sueila Silva Carvalho '
         ],
     },
     {
         title: 'COMISSÃO DE DIREITO DA PESSOA NEURODIVERGENTE',
         presidente: 'Dra. Tatiane Cristina Camargo Ferreira',
         emailPresidente: 'tccamargof@gmail.com',
-        membrosEfetivos: [],
+        vicePresidente: 'Dra. Nathalia Loures Capelo ',
+        membrosEfetivos: [
+            'Dra. Jaqueline Renata dos Santos de Oliveira ',
+            'Dra. Norberto Gil Ribeiro '
+        ],
     }
 ];
 
 const templateAccordion = ({
     title,
     presidente,
-    emailPresidente,
     vicePresidente,
     instagramLink,
     membrosEfetivos
@@ -319,7 +283,6 @@ const templateAccordion = ({
                 <p class="mt-1">
                     ${presidente || ''}
                     <br>
-                    E-mail: ${emailPresidente || ''}
                 </p>
 
                 ${vicePresidente
@@ -336,7 +299,11 @@ const templateAccordion = ({
                         <span class="accordion-body-title2">Redes Sociais</span><br>
                         
                         <a href="${instagramLink || ''}" target="_blank">
-                            <img src="assets/icons/icon-Instagram.svg" width="20px">
+                            <svg width="20" height="20" style="margin-top: 5px;" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M11.5418 3.97674C7.45022 3.97674 4.04022 7.4022 4.04022 11.7442V31.2558C4.04022 35.5978 7.45022 39.0233 11.5418 39.0233H30.5217C34.6133 39.0233 38.0233 35.5978 38.0233 31.2558V11.7442C38.0233 7.4022 34.6133 3.97674 30.5217 3.97674H11.5418ZM0.0634766 11.7442C0.0634766 5.3102 5.15106 0 11.5418 0H30.5217C36.9124 0 42 5.3102 42 11.7442V31.2558C42 37.6898 36.9124 43 30.5217 43H11.5418C5.15106 43 0.0634766 37.6898 0.0634766 31.2558V11.7442Z" fill="#3739FE"/>
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M22.2313 15.1998C20.8958 15.0031 19.5324 15.2298 18.3355 15.8466C17.1388 16.4634 16.1709 17.4378 15.5671 18.6292C14.9635 19.8205 14.7538 21.1692 14.9669 22.4843C15.1799 23.7994 15.8054 25.0164 16.7568 25.9614C17.7084 26.9065 18.9376 27.5313 20.2705 27.7443C21.6034 27.9573 22.9695 27.7473 24.174 27.1451C25.3784 26.5431 26.3584 25.5804 26.9769 24.3964C27.5954 23.2126 27.8218 21.8665 27.6251 20.5489C27.4244 19.2049 26.7936 17.9585 25.8231 16.9947C24.8525 16.0306 23.5935 15.4004 22.2313 15.1998ZM16.5138 12.3117C18.4506 11.3135 20.6539 10.9478 22.8107 11.2655C25.0108 11.5895 27.0497 12.608 28.6255 14.1731C30.2015 15.7384 31.2306 17.7676 31.5582 19.9617C31.8794 22.1128 31.5093 24.3091 30.5017 26.2378C29.4942 28.1663 27.9015 29.7277 25.9522 30.7022C24.0032 31.6765 21.7956 32.0152 19.6428 31.6712C17.49 31.3271 15.4992 30.3173 13.9544 28.7829C12.4094 27.2484 11.3891 25.2671 11.0413 23.1203C10.6935 20.9733 11.0363 18.7726 12.0199 16.8316C13.0034 14.8909 14.5767 13.31 16.5138 12.3117Z" fill="#3739FE"/>
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M28.7141 9.4202C28.7141 8.17231 29.7257 7.16069 30.9736 7.16069H30.9956C32.2435 7.16069 33.2551 8.17231 33.2551 9.4202C33.2551 10.6681 32.2435 11.6797 30.9956 11.6797H30.9736C29.7257 11.6797 28.7141 10.6681 28.7141 9.4202Z" fill="#3739FE"/>
+</svg>
                         </a>
                     `
             : ''
--- assets/js/default.js
+++ assets/js/default.js
@@ -10,7 +10,7 @@ const navbarHtml = `
         <div class="container-fluid">
             
             <a class="navbar-brand ms-md-5" href="index.html">
-                <img src="assets/images/logo-OAB.png" alt="OAB-jandira" class="logo-oab">
+                <img src="assets/images/logoOab.svg" alt="OAB-jandira" class="logo-oab">
             </a>
     
             
@@ -27,7 +27,7 @@ const navbarHtml = `
             </button>
     
             
-            <div class="collapse navbar-collapse justify-content-end me-4" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                 <ul class="navbar-nav" style="color: #1e2b37; font-weight: 500 ;">
                     <li class="nav-item">
                         <a class="nav-link ${(path.includes('index') || path == '/') ? 'active' : ''}" href="index.html">
@@ -66,13 +66,21 @@ const navbarHtml = `
                         <a class="nav-link ${path.includes('contato') ? 'active' : ''}" href="contato.html">Contato</a>
                     </li>
                     <li>
-                        <a href="https://www.facebook.com/oabsaopaulo/?locale=pt_BR" class="nav-item" target="_blank">
-                            <img style="width: 30px; margin-right: 20px; padding-top: 5px;" src="assets/icons/icon-Facebook.svg" alt="Facebook">
+                        <a href="https://www.facebook.com/oabsaopaulo/?locale=pt_BR" class="nav-item svg" target="_blank">
+                               <svg width="30" height="30" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M11.6053 0C5.21454 0 0.126953 5.3102 0.126953 11.7442V31.2558C0.126953 37.6898 5.21454 43 11.6053 43H30.5852C36.9759 43 42.0635 37.6898 42.0635 31.2558V11.7442C42.0635 5.3102 36.9759 0 30.5852 0H11.6053ZM4.10369 11.7442C4.10369 7.4022 7.51369 3.97674 11.6053 3.97674H30.5852C34.6767 3.97674 38.0867 7.4022 38.0867 11.7442V31.2558C38.0867 35.5978 34.6767 39.0233 30.5852 39.0233H26.4127V26.0741H29.8412L32.127 20.3333H26.4127V17.1185C26.4127 16.3148 26.9841 15.7407 27.7841 15.7407H30.9841V10H26.4127C23.2127 10 20.6984 12.5259 20.6984 15.7407V20.3333H16.127V26.0741H20.6984V39.0233H11.6053C7.51369 39.0233 4.10369 35.5978 4.10369 31.2558V11.7442Z" fill="white"/>
+</svg>
+
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.instagram.com/oab.jandira/" class="nav-item ml-2" target="_black">
-                            <img style="width: 30px; padding-top: 5px;" src="assets/icons/icon-Instagram.svg" alt="Instagram">
+                        <a href="https://www.instagram.com/oab.jandira/" class="nav-item svg ml-2" target="_black">
+                            <svg width="30" height="30" viewBox="0 0 42 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M11.5418 3.97674C7.45022 3.97674 4.04022 7.4022 4.04022 11.7442V31.2558C4.04022 35.5978 7.45022 39.0233 11.5418 39.0233H30.5217C34.6133 39.0233 38.0233 35.5978 38.0233 31.2558V11.7442C38.0233 7.4022 34.6133 3.97674 30.5217 3.97674H11.5418ZM0.0634766 11.7442C0.0634766 5.3102 5.15106 0 11.5418 0H30.5217C36.9124 0 42 5.3102 42 11.7442V31.2558C42 37.6898 36.9124 43 30.5217 43H11.5418C5.15106 43 0.0634766 37.6898 0.0634766 31.2558V11.7442Z" fill="white"/>
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M22.2313 15.1998C20.8958 15.0031 19.5324 15.2298 18.3355 15.8466C17.1388 16.4634 16.1709 17.4378 15.5671 18.6292C14.9635 19.8205 14.7538 21.1692 14.9669 22.4843C15.1799 23.7994 15.8054 25.0164 16.7568 25.9614C17.7084 26.9065 18.9376 27.5313 20.2705 27.7443C21.6034 27.9573 22.9695 27.7473 24.174 27.1451C25.3784 26.5431 26.3584 25.5804 26.9769 24.3964C27.5954 23.2126 27.8218 21.8665 27.6251 20.5489C27.4244 19.2049 26.7936 17.9585 25.8231 16.9947C24.8525 16.0306 23.5935 15.4004 22.2313 15.1998ZM16.5138 12.3117C18.4506 11.3135 20.6539 10.9478 22.8107 11.2655C25.0108 11.5895 27.0497 12.608 28.6255 14.1731C30.2015 15.7384 31.2306 17.7676 31.5582 19.9617C31.8794 22.1128 31.5093 24.3091 30.5017 26.2378C29.4942 28.1663 27.9015 29.7277 25.9522 30.7022C24.0032 31.6765 21.7956 32.0152 19.6428 31.6712C17.49 31.3271 15.4992 30.3173 13.9544 28.7829C12.4094 27.2484 11.3891 25.2671 11.0413 23.1203C10.6935 20.9733 11.0363 18.7726 12.0199 16.8316C13.0034 14.8909 14.5767 13.31 16.5138 12.3117Z" fill="white"/>
+<path class="navbar-svg" fill-rule="evenodd" clip-rule="evenodd" d="M28.7141 9.4202C28.7141 8.17231 29.7257 7.16069 30.9736 7.16069H30.9956C32.2435 7.16069 33.2551 8.17231 33.2551 9.4202C33.2551 10.6681 32.2435 11.6797 30.9956 11.6797H30.9736C29.7257 11.6797 28.7141 10.6681 28.7141 9.4202Z" fill="white"/>
+</svg>
+
                         </a>
                     </li>
                         
@@ -90,7 +98,7 @@ document.getElementById("navbar").innerHTML = navbarHtml;
 const header = `
     <div class="header">
         <marquee scrollamount="1" scrolldelay="20" truespeed="20">
-            <a href="https://www2.oabsp.org.br/asp/dotnet/LoginSite/LoginMain.aspx" class="no-link-style" target="_blank">
+            <a href="contato.html" class="no-link-style" target="_blank">
                 <p style="padding-top: 0.5em;">Fale com a <label
                         style="font-weight: bold;"> OAB Jandira</label> Tel.: <label style="font-weight: bold;">(11) 4789-0979</label>
                 </p>
